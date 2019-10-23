161bf024b7627c28eff43e648a2f2e37f7317d58
tcotter1432 <tcotter1432@gmail.com>
2019-10-23 09:27:46 -0400

    Updates made.

--- Lab8Arrays/Program.cs
+++ Lab8Arrays/Program.cs
@@ -4,11 +4,11 @@ namespace Lab8Arrays
 {
     class Program
     {
-        static string[] Students = {"Link", "Ryne", "Male Corrin", "DoomGuy", "Geralt", "The Emperor of Mankind", "Chuck Norris", "Ganondorf", "Princess Zelda", "Female Corrin"};
-        static string[] FavoriteWeapon= { "The Master Sword", "A Pair of Daggers", "the Yato", "A Shotgun", "A set of Steel and Silver Swords", "The Legiones Astartes", "a Roundhouse Kick", "a Sword", "a Bow", "the Yato" };
-        static string[] HomeWorld = { "Hyrule", "The First", "Valla", "????", "The Witcher World", "Holy Terra", "Earth", "Hyrule", "Hyrule", "Valla" };
-        static string[] BestFriend = { "Princess Zelda", "Thancred", "Beruka", "Ammuntion", "Triss Merigold", "????", "God", "N/A", "Link", "Silas" };
-        static string[] MortalEnemy = { "Ganondorf", "The Light", "Anankanos", "Hell", "The Wild Hunt", "Horus", "no one living", "Link", "Ganondorf", "Anankanos" };
+        static string[] students = {"Link", "Ryne", "Male Corrin", "DoomGuy", "Geralt", "The Emperor of Mankind", "Chuck Norris", "Ganondorf", "Princess Zelda", "Female Corrin"};
+        static string[] favoriteWeapon= { "The Master Sword", "A Pair of Daggers", "the Yato", "A Shotgun", "A set of Steel and Silver Swords", "The Legiones Astartes", "a Roundhouse Kick", "a Sword", "a Bow of Light", "the Yato" };
+        static string[] homeWorld = { "Hyrule", "The First", "Valla", "????", "The Witcher World", "Holy Terra", "Earth", "Hyrule", "Hyrule", "Valla" };
+        static string[] bestFriend = { "Princess Zelda", "Thancred", "Beruka", "Ammuntion", "Triss Merigold", "????", "God", "N/A", "Link", "Silas" };
+        static string[] mortalEnemy = { "Ganondorf", "The Light", "Anankanos", "Hell", "The Wild Hunt", "Horus", "no one living", "Link", "Ganondorf", "Anankanos" };
         static int userIndex;
         static void Main(string[] args)
         {
@@ -17,28 +17,28 @@ namespace Lab8Arrays
             Console.WriteLine("Welcome to the Inter-Dimensional C# GC Coding Bootcamp!");
             while (keepTrawling)
             {
-                userIndex = ValidateRange($"Who would you like to know about? (1,{Students.Length})", 0, Students.Length);
+                userIndex = ValidateRange($"Who would you like to know about? (1,{students.Length})", 0, students.Length);
                 if (userIndex == -1)
                 {
                     Console.WriteLine("===Students===");
-                    ListOutArray(Students);
+                    ListOutArray(students);
                     Console.WriteLine("");
                     Console.WriteLine("===Weapons===");
-                    ListOutArray(FavoriteWeapon);
+                    ListOutArray(favoriteWeapon);
                     Console.WriteLine("");
                     Console.WriteLine("===Home Worlds===");
-                    ListOutArray(HomeWorld); 
+                    ListOutArray(homeWorld); 
                     Console.WriteLine("");
                     Console.WriteLine("===Best Friends===");
-                    ListOutArray(BestFriend);
+                    ListOutArray(bestFriend);
                     Console.WriteLine("");
                     Console.WriteLine("===Mortal Enemies===");
-                    ListOutArray(MortalEnemy);
+                    ListOutArray(mortalEnemy);
                     Console.WriteLine("");
 
                     continue;
                 }
-                Console.WriteLine($"Student number {userIndex} is {Students[userIndex - 1]}!");
+                Console.WriteLine($"Student number {userIndex} is {students[userIndex - 1]}!");
                 //Console.WriteLine("What would you like to know about them? (FavoriteWeapon, Homeworld, BestFriend)");
                 CheckInfoInput();
 
@@ -93,20 +93,26 @@ namespace Lab8Arrays
         }
         public static void CheckInfoInput()
         {
-            string userInput = GetUserInput("What would you like to know about them? 1. Favorite Weapon \n 2. Homeworld \n 3. Best Friend \n 4. Mortal Enemy").ToLower();
+            string userInput = GetUserInput("What would you like to know about them? \n 1. Favorite Weapon \n 2. Homeworld \n 3. Best Friend \n 4. Mortal Enemy \n 5. Different Student").ToLower();
             switch (userInput)
             {
                 case ("1"):
-                    Console.WriteLine($"{Students[userIndex-1]}'s favorite weapon is {FavoriteWeapon[userIndex - 1]}!");
+                    Console.WriteLine($"{students[userIndex-1]}'s favorite weapon is {favoriteWeapon[userIndex - 1]}!");
+                    CheckInfoInput();
                     break;
                 case ("2"):
-                    Console.WriteLine($"{Students[userIndex - 1]}'s home world is {HomeWorld[userIndex - 1]}!");
+                    Console.WriteLine($"{students[userIndex - 1]}'s home world is {homeWorld[userIndex - 1]}!");
+                    CheckInfoInput();
                     break;
                 case ("3"):
-                    Console.WriteLine($"{Students[userIndex - 1]}'s best friend is {BestFriend[userIndex - 1]}!");
+                    Console.WriteLine($"{students[userIndex - 1]}'s best friend is {bestFriend[userIndex - 1]}!");
+                    CheckInfoInput();
                     break;
                 case ("4"):
-                    Console.WriteLine($"{Students[userIndex - 1]}'s mortal enemy is {MortalEnemy[userIndex - 1]}!");
+                    Console.WriteLine($"{students[userIndex - 1]}'s mortal enemy is {mortalEnemy[userIndex - 1]}!");
+                    CheckInfoInput();
+                    break;
+                case ("5"):
                     break;
                 default:
                     Console.WriteLine("Invalid input...");
